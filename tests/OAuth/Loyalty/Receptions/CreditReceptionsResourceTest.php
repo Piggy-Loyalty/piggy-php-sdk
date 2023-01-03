9b29ee400fef7b50f4ea847b3b7c6d06e975e35c
Teun Koster <teun@letstalk.nl>
2023-01-03 15:54:52 +0100

    Added custom attributes to creditReceptiion test

--- tests/OAuth/Loyalty/Receptions/CreditReceptionsResourceTest.php
+++ tests/OAuth/Loyalty/Receptions/CreditReceptionsResourceTest.php
@@ -35,10 +35,11 @@ class CreditReceptionsResourceTest extends OAuthTestCase
             "unit" => [
                 "name" => 'bonkers',
                 "label" => "Bonkers"
-            ]
+            ],
+            'foo' => 'bar'
         ]);
 
-        $creditReception = $this->mockedClient->creditReceptions->create('123-312', '123-123', 10);
+        $creditReception = $this->mockedClient->creditReceptions->create('123-312', '123-123', 10, null, null, null, null, ['foo' => 'bar']);
 
         $this->assertEquals("credit_reception", $creditReception->getType());
         $this->assertEquals(101, $creditReception->getCredits());
@@ -50,5 +51,6 @@ class CreditReceptionsResourceTest extends OAuthTestCase
         $this->assertEquals(1011, $creditReception->getUnitValue());
         $this->assertEquals('bonkers', $creditReception->getUnit()->getName());
         $this->assertEquals("Bonkers", $creditReception->getUnit()->getLabel());
+        $this->assertEquals(['foo' => 'bar'], $creditReception->getAttributes());
     }
 }
