b224719f53ed8e6b3c22f4cfd0355a47482c9711
Michael Zonneveld <michael.zonneveld@piggy.nl>
2023-12-19 17:00:05 +0100

    removed blank line

--- src/Resources/OAuth/Vouchers/PromotionAttributesResource.php
+++ src/Resources/OAuth/Vouchers/PromotionAttributesResource.php
@@ -11,7 +11,6 @@ use Piggy\Api\Resources\BaseResource;
 /**
  * Class ContactAttributesResource
  * @package Piggy\Api\Resources\OAuth\Vouchers
- 
  */
 class PromotionAttributesResource extends BaseResource
 {
