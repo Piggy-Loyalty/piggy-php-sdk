4dde30b55ff5de3eaf80f0a8ef913d9970b365b1
Diego Relyveld <diego@piggy.nl>
2024-10-03 14:26:39 +0200

    Add new GiftcardProgram properties to GiftcardProgramsResourceTest

--- tests/OAuth/Giftcards/Program/GiftcardProgramsResourceTest.php
+++ tests/OAuth/Giftcards/Program/GiftcardProgramsResourceTest.php
@@ -14,25 +14,37 @@ class GiftcardProgramsResourceTest extends OAuthTestCase
     {
         $this->addExpectedResponse([
             [
+                'uuid' => '123123',
                 'name' => 'My Giftcard Program Test',
                 'active' => true,
-                'uuid' => '123123',
+                'max_amount_in_cents' => 10000,
+                'calculator_flow' => 'default',
+                'expiration_days' => 365,
             ],
             [
+                'uuid' => '321321',
                 'name' => 'My Old Giftcard Program Test',
                 'active' => false,
-                'uuid' => '321321',
+                'max_amount_in_cents' => 5000,
+                'calculator_flow' => 'overlay',
+                'expiration_days' => 180,
             ],
         ]);
 
         $giftcardPrograms = $this->mockedClient->giftcardProgram->list();
 
+        $this->assertEquals('123123', $giftcardPrograms[0]->getUuid());
         $this->assertEquals('My Giftcard Program Test', $giftcardPrograms[0]->getName());
         $this->assertEquals(true, $giftcardPrograms[0]->isActive());
-        $this->assertEquals('123123', $giftcardPrograms[0]->getUuid());
+        $this->assertEquals(10000, $giftcardPrograms[0]->getMaxAmountInCents());
+        $this->assertEquals('default', $giftcardPrograms[0]->getCalculatorFlow());
+        $this->assertEquals(365, $giftcardPrograms[0]->getExpirationDays());
 
+        $this->assertEquals('321321', $giftcardPrograms[1]->getUuid());
         $this->assertEquals('My Old Giftcard Program Test', $giftcardPrograms[1]->getName());
         $this->assertEquals(false, $giftcardPrograms[1]->isActive());
-        $this->assertEquals('321321', $giftcardPrograms[1]->getUuid());
+        $this->assertEquals(5000, $giftcardPrograms[1]->getMaxAmountInCents());
+        $this->assertEquals('overlay', $giftcardPrograms[1]->getCalculatorFlow());
+        $this->assertEquals(180, $giftcardPrograms[1]->getExpirationDays());
     }
 }
